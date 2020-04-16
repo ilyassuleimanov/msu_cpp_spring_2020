f9fdca8619fe0573a90c495b33db419ae74ed8ae
ilyassuleimanov <ilyassuleimanov@gmail.com>
2020-04-16 03:25:54 +0300

    hw6

--- 06/main.cpp
+++ 06/main.cpp
@@ -2,16 +2,16 @@
 
 int main() {
 	std::string string = format("abcd {} {}.", 1,"abcd", 1);
-    assert(string == "abcd 1 abcd.");
-    std::cout << "\nTest 1 passed";
-    
-    string = format("");
-    assert(string == "");
-    std::cout << "\nTest 2 passed";
-    
-    string = format("abcd {3} {1}.", 1, "abcd", 1, 4);
-    assert(string == "abcd 4 abcd.");
-    std::cout << "\nTest 3 passed.";
-    std::cout << "\nAll tests successfully passed\n"; 
+	assert(string == "abcd 1 abcd.");
+	std::cout << "\nTest 1 passed";
+	
+	string = format("");
+	assert(string == "");
+	std::cout << "\nTest 2 passed";
+	
+	string = format("abcd {3} {1}.", 1, "abcd", 1, 4);
+	assert(string == "abcd 4 abcd.");
+	std::cout << "\nTest 3 passed.";
+	std::cout << "\nAll tests successfully passed\n"; 
 	return 0;
 }
