f1ff1dea1c20d90c426d2107669675eb8be7ef9c
ilyassuleimanov <ilyassuleimanov@gmail.com>
2020-05-06 22:58:11 +0300

    hw8

---
+++ 08/ThreadPool.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <future>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <cassert>
+#include <cstring>
+#pragma once
+
+class ThreadPool {
+	std::vector<std::thread> threads;
+	std::queue<std::function<void()>> tasks;
+	std::mutex mut;
+	std::condition_variable cv;
+	bool dead = false;
+	
+	public:
+		ThreadPool(size_t poolSize) {
+			for (size_t i = 0; i < poolSize; ++i) {
+				threads.emplace_back([this]() {
+					while(true) {
+						std::unique_lock<std::mutex> lock(mut);
+						cv.wait(lock, 
+						[this] () {
+							return !(!dead && tasks.empty());
+						});
+						if (dead) return;
+						auto task = tasks.front();
+						tasks.pop();
+						task();
+					}
+				});
+			}
+		}
+		~ThreadPool() {
+			dead = true;
+			cv.notify_all();
+			for (auto& t : threads)
+				t.join();
+		}
+	    template <class Func, class... Args>
+	    auto exec(Func func, Args... args) -> std::future<decltype(func(args...))>{
+	        std::unique_lock<std::mutex> lock(mut);
+	        auto task = std::make_shared<std::packaged_task<decltype(func(args...))()>>(std::bind(func, args...));
+	        tasks.push([task](){(*task)();});
+	        cv.notify_one();
+	        return task->get_future();
+	    }
+};
---
+++ 08/test.cpp
@@ -0,0 +1,31 @@
+#include "ThreadPool.h"
+
+int f1(int a) {
+	std::cout << "Test number 1\n";
+    return a * 10;
+}
+
+int f2(int a, int b, int c) {
+	std::cout << "Test number 2\n";
+    return (a + b - c) * 10;
+}
+
+int f3(std::string str){
+	std::cout << "Test number 3\n";
+    return str.length();
+}
+
+int main(){
+	std::cout << "Starting tests\n";
+    ThreadPool pool(3);
+    auto task1 = pool.exec(f1, 22);
+    int test1 = task1.get();
+    assert(test1 == 220);
+    auto task2 = pool.exec(f2, 10, 15, 5);
+    int test2 = task2.get();
+    assert(test2 == 200);
+    auto task3 =pool.exec(f3, "the last test");
+    int test3 = task3.get();
+    assert(test3 == 13);
+    std::cout << "All tests passed\n";
+}
